9ad961e71ad849b662409812d369dc4760559b9a
HasnainAliPK <hasnain2640@hotmail,com>
2024-12-15 12:31:40 +0100

    NY igen

--- CoffeeCampus/Pakistani.cs
+++ CoffeeCampus/Pakistani.cs
@@ -6,7 +6,7 @@
         public string description { get; set; }
             
         public string type { get; set; }
-        public string description_type { get; set;
+        public string description_type { get; set; }
         public Pakistani() { }
         
     }
