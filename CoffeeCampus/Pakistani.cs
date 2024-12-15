41494da89717d01ad5305660329c11d041298f14
HasnainAliPK <hasnain2640@hotmail,com>
2024-12-15 12:29:07 +0100

    Pakistani ny kode

--- CoffeeCampus/Pakistani.cs
+++ CoffeeCampus/Pakistani.cs
@@ -3,5 +3,12 @@
     public class Pakistani
     {
         public string name {  get; set; }
+        public string description { get; set; }
+            
+        public string type { get; set; }
+        public string description_type { get; set;
+        public Pakistani() { }
+        
+    }
     }
 }
